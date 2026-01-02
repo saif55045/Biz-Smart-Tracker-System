62d5283b1f6565a2487b633f75b2e0a924277021
saif55045 <su3561116@gmail.com>
2026-01-02 20:34:18 +0500

    frontend error fixed

--- Client/pages/user/UserManager.jsx
+++ Client/pages/user/UserManager.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from 'react';
 import axios from 'axios';
 import { useCurrency } from '../../context/CurrencyContext';
 import config from '../../src/config';
-import '../../src/assets/styles/userManager.css';
+import './UserManager.css';
 
 // Icons
 const PlusIcon = () => (
